a7f8b3b0d0fba9b293059177c848d3c7c4530d4a
VincenzoZ24 <zitov287@gmail.com>
2023-01-12 17:50:42 +0100

    creato Hello World con blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,7 @@
+<?php
+$titolo = 'Hello World';
+?>
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -81,19 +85,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <h1>{{$titolo}}</h1>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            
             </div>
         </div>
     </body>
